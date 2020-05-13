789145355026c5c856b2ce3983b39fad4ef8bafd
Bart6114 <bart@dploy.ai>
2020-05-13 13:35:30 +0200

    fix deps variable shadowing

--- dploy_kickstart/deps.py
+++ dploy_kickstart/deps.py
@@ -22,6 +22,13 @@ def install_requirements_txt(requirements_txt_location: str) -> None:
     """Install requirements from a requirements.txt file."""
     # see https://pip.pypa.io/en/latest/user_guide/#using-pip-from-your-program
     # need to do sys call
+    if not requirements_txt_location.endswith("requirements.txt"):
+        raise RequirementsInstallException(
+            "{} - should contain a requirements.txt reference".format(
+                requirements_txt_location
+            )
+        )
+
     cmd = "{} -m pip install -r {}".format(sys.executable, requirements_txt_location)
     print("##")
     print(cmd)
@@ -34,6 +41,11 @@ def install_setup_py(setup_py_location: str) -> None:
     """Install requirements from a setup.py file."""
     # see https://pip.pypa.io/en/latest/user_guide/#using-pip-from-your-program
     # need to do sys call
+    if not setup_py_location.endswith("setup.py"):
+        raise SetupPyInstallException(
+            "{} - should contain a setup.py reference".format(setup_py_location)
+        )
+
     setup_py_dir_location = os.path.dirname(setup_py_location)
 
     cmd = "{} -m pip install {}".format(sys.executable, setup_py_dir_location)
@@ -49,7 +61,7 @@ def execute_cmd(cmd: str, wd: str = None) -> int:
     # subprocess.CalledProcessError doesnt return the stderr
     # of the pip call itself (it just says it fails)
     # so we solve it with popen and pipe/capture the output
-    p = subprocess.run(cmd, stdout=sys.stdout, stderr=sys.stderr, shell=True, cwd=wd)
+    p = subprocess.run(cmd, shell=True, cwd=wd)
     # output, error = p.communicate()
     return p.returncode
     if p.returncode != 0:
