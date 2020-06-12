2f58a780241032cfaacfa91f1d834db1c91c7abb
Bart6114 <bart@dploy.ai>
2020-06-12 08:14:13 +0200

    bump version

--- dploy_kickstart/__init__.py
+++ dploy_kickstart/__init__.py
@@ -1,7 +1,7 @@
 """Kickstart your functions."""
 from dploy_kickstart.cmd import cli
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 if __name__ == "__main__":
     cli()
--- tests/test_dploy_kickstart.py
+++ tests/test_dploy_kickstart.py
@@ -2,4 +2,4 @@ from dploy_kickstart import __version__
 
 
 def test_version():
-    assert __version__ == "0.1.4"
+    assert __version__ == "0.1.5"
