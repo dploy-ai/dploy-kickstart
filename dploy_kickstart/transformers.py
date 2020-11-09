2d38d8103822deba21c4f89a5cf8aba5b7879f34
baturayo <baturay@dataroots.io>
2020-11-09 15:30:13 +0100

    Add numpy data type support for annotated methods

--- dploy_kickstart/transformers.py
+++ dploy_kickstart/transformers.py
@@ -13,13 +13,21 @@ def bytes_io_resp(func_result: typing.Any) -> Response:
     return Response(func_result.getvalue(), mimetype="application/octet-stream")
 
 
-def default_req(f: da.AnnotatedCallable, req: Request) -> typing.Any:
-    return f(req.data)
+def np_tolist_resp(func_result: typing.Any) -> Response:
+    return jsonify(func_result.tolist())
+
+
+def np_item_resp(func_result: typing.Any) -> Response:
+    return jsonify(func_result.item())
 
 
 def json_resp(func_result: typing.Any) -> Response:
     """Transform json response."""
     return jsonify(func_result)
+
+
+def default_req(f: da.AnnotatedCallable, req: Request) -> typing.Any:
+    return f(req.data)
 
 
 def json_req(f: da.AnnotatedCallable, req: Request) -> typing.Any:
@@ -40,4 +48,19 @@ MIME_TYPE_RES_MAPPER = {
     "dict": json_resp,
     "bytes": bytes_resp,
     "BytesIO": bytes_io_resp,
+
+    # Numpy Return dtypes
+    "ndarray": np_tolist_resp,
+    "matrix": np_tolist_resp,
+    "int8": np_item_resp,
+    "uint8": np_item_resp,
+    "int16": np_item_resp,
+    "uint16": np_item_resp,
+    "int32": np_item_resp,
+    "uint32": np_item_resp,
+    "int64": np_item_resp,
+    "uint64": np_item_resp,
+    "float16": np_item_resp,
+    "float32": np_item_resp,
+    "float64": np_item_resp,
 }
