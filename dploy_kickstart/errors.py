a68eef29356ed97bbce8298852672eab5eb91581
baturayo <baturay@dataroots.io>
2020-11-24 12:11:08 +0100

    Fix errors.py, raise more explicit errors

--- dploy_kickstart/errors.py
+++ dploy_kickstart/errors.py
@@ -16,7 +16,6 @@ class ScriptImportError(ServerException):
     status_code = 500
 
     def __init__(self, message: str):
-        super().__init__(self)
         self.message = message
 
     def to_dict(self) -> dict:
@@ -27,7 +26,6 @@ class UnsupportedEntrypoint(ServerException):
     status_code = 500
 
     def __init__(self, entrypoint: str):
-        super().__init__(self)
         self.message = f"entrypoint '{entrypoint}' not supported"
 
     def to_dict(self) -> dict:
